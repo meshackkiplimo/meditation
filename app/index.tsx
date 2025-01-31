04f53fa78ffdfa6be5a91ebc13d964a40e74f76e
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2025-01-31 21:41:17 +0300

    fix:added a bg imahe

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,56 @@
-import {  StyleSheet, View,Text} from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, SafeAreaView } from 'react-native';
+import React from 'react';
+import beachImage from '../assets/meditation-images/beach.webp';
+import { LinearGradient } from 'expo-linear-gradient';
 
-
-
-export default function HomeScreen() {
+const App = () => {
   return (
-    <View className='flex-1 justify-center items-center text-red-700 bg-blue-600  ' >
-
-      <Text className='text-2xl font-bold text-green-500' >Hello World</Text>
-    </View>
-     
-    
+    <ImageBackground 
+      source={beachImage} 
+      resizeMode="cover" 
+      style={styles.background}
+    >
+      <LinearGradient
+        colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
+        style={styles.gradient}
+      >
+        <SafeAreaView style={styles.safeArea}>
+          <View style={styles.container}>
+            <Text style={styles.text}>Simple Meditation App</Text>
+            <Text className='text-center text-white' >Made with ❤️</Text>
+          </View>
+        </SafeAreaView>
+      </LinearGradient>
+    </ImageBackground>
   );
-}
+};
 
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',  // Aligns the content to the top of the screen
+    alignItems: 'center',
+    paddingTop: 50,  // Optional: Adds some space from the top
+  },
+  text: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  gradient: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  safeArea: {
+    flex: 1,
+  },
+});
 
-  
-
+export default App;
