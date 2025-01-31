918099b5b791fb05703ee747126a15f7bf4420e1
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2025-01-31 18:01:20 +0300

    fix:tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import {  StyleSheet, View,Text} from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View className='flex-1 justify-center items-center text-red-700 bg-blue-600 ' >
+    <View className='flex-1 justify-center items-center text-red-700 bg-blue-600  ' >
 
       <Text className='text-2xl font-bold text-green-500' >Hello World</Text>
     </View>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    content: [
       "./App.{js,jsx,ts,tsx}",
-      "./app/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the app folder
-      "./components/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the components folder]
-      "./app/(tabs)/meditate.tsx",
-  ],
-  theme: {
+      "./app/**/*.{js,jsx,ts,tsx}",  // Include all JS, JSX, TS, and TSX files in the app folder
+      "./components/**/*.{js,jsx,ts,tsx}",  // Include all JS, JSX, TS, and TSX files in the components folder
+      "./app/(tabs)/meditate.tsx",  // Specific file path
+    ],
+    theme: {
       extend: {
-          fontFamily: {
-              rmono: ["Roboto-Mono", "sans-serif"],
-          },
+        fontFamily: {
+          rmono: ["Roboto-Mono", "sans-serif"],  // Custom font family
+        },
       },
-  },
-  plugins: [],
-};
+    },
+    plugins: [],
+  };
+  
